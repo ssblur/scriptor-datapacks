7eeada9a84cf69556904db38681c234329c347b2
Patrick Hallbick <patrick.hallbick@outlook.com>
2024-02-01 09:52:44 -0500

    Add Forge compatibility

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 for d in scriptor_*/ ; do
-    git archive --format=zip HEAD:$d > ${d::-1}.zip
-    cp ${d::-1}.zip ${d::-1}.jar
+    d=${d::-1}
+    t=${d}_tmp
+    cp -R $d $t
+    cp -R mod_resources/* $t
+    
+    jar cf $d.jar -C $t .
+
+    cd $t
+    rm -rf src
+    rm -rf META-INF
+    rm fabric.mod.json
+    cd ..
+
+    jar cf $d.zip -C $t .
+
+    rm -rf $t
 done
